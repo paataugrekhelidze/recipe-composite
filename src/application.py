d3329ea3f3da859c3cc4e850d61e1fa93d3b609f
Paata Ugrekhelidze <paataugrekhelidze@gmail.com>
2022-12-13 18:40:20 -0500

    require authentication as middleware

--- src/application.py
+++ src/application.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(compositer)
+@app.before_request
+def before_request():
+    logged_in = requests.get(os.environ["api_endpoint"] + "/auth/is_logged_in").status_code==200
+    if not logged_in:
+        return Response("NOT AUTHENTICATED", status=401, content_type="text/plain")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5015, debug=True)
